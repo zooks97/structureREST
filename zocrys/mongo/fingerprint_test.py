188e626ec944a854c3518283b3ec6a17b9a36336
Austin Zadoks <azadoks@structureREST.epfl.ch>
2018-06-07 17:00:49 +0200

    Removed structure tidy fingerprint insertion from fingerprint test

--- zocrys/mongo/fingerprint_test.py
+++ zocrys/mongo/fingerprint_test.py
@@ -67,7 +67,7 @@ matminer_fingerprints = fingerprints.matminer_fingerprints(structures)
 logging.info('Adding fingerprints to documents')
 for d, document in enumerate(documents):
     documents[d]['matminer_fingerprint'] = matminer_fingerprints[d]
-    documents[d]['stidy_fingerprint'] = stidy_fingerprints[d]
+#     documents[d]['stidy_fingerprint'] = stidy_fingerprints[d]
 
 logging.info('Inserting {} documents into {}'.format(
     len(documents), OUTPUT_COLLECTION))
