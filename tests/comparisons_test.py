5d461cb1d18857d38c058f5190e693bc597f69cb
Austin Zadoks <zooks97@gmail.com>
2018-07-09 09:14:08 +0200

    Creating tests

---
+++ tests/comparisons_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestMatminerComparisons(unittest.TestCase):
+
+    def test_same(self):
+        self.assertTrue()
+
+    def test_different(self):
+        self.assertFalse()
+
+
+class TestPymatgenComparisons(unittest.TestCase):
+
+    def test_same(self):
+        self.assertTrue()
+
+    def test_different(self):
+        self.assertTrue()
+
